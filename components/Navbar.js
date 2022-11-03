2dab472e29c00636320c34974b234eac41c2d2e8
rehanheeeeeeeee <rehantosif4@gmail.com>
2022-11-04 01:40:23 +0300

    fdff

--- components/Navbar.js
+++ components/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar() {
           <Image src="/download.svg" width={35} height={35} alt="" />
           <p className="font-bold text-white">OpenSea</p>
         </div>
-        <div className="flex-1 flex-row items-end">
+        <div className="flex-1 flex-row items-end max-sm:ml-1">
           <div className={styles.searchBar}>
             <MagnifyingGlassIcon className="w-5 text-gray-200" />
             <input
